0fe698b33c31716a9be54c80d3ef72571c197aa9
Felix Jablinski <jablinskifelix@gmail.com>
2024-04-13 22:54:11 +0200

    #1 Add clock as default

--- src/api/main.py
+++ src/api/main.py
@@ -2,11 +2,13 @@ from fastapi import APIRouter
 
 from api.handler import AppHandler
 from api.routes import account, apps
-
+from apps.clock.clock import Clock
 
 api_router = APIRouter()
 
 api_router.include_router(account.router, prefix="/account", tags=["account"])
 api_router.include_router(apps.router, prefix="/apps", tags=["apps"])
 
-app_handler = AppHandler()
+
+clock = Clock()
+app_handler = AppHandler(clock)
