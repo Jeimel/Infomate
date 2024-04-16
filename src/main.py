5a1e6f9ded634da2806e1d715238a1c5ee07d142
Felix Jablinski <jablinskifelix@gmail.com>
2024-04-16 22:54:49 +0200

    #1 Remove unused imports

--- src/main.py
+++ src/main.py
@@ -1,28 +1,18 @@
 from os import chdir
 from pathlib import Path
+
 chdir(Path(__file__).parent.resolve())
 
 from uvicorn import run
 from fastapi import FastAPI
-from contextlib import asynccontextmanager
 from sys import exit
 
 from api.main import api_router
-from api.handler import AppHandler
 from api.routes.apps import lifespan
 
 
 API_ROUTE = "/api"
 
-
-# app_handler = AppHandler()
-
-"""
-@asynccontextmanager
-async def lifespan(_: FastAPI):
-    create_task(app_handler.start())
-    yield
-"""
 
 app = FastAPI(title="Infomate", lifespan=lifespan)
 
