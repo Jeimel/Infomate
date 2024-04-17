33920b2f05bd7191923c58ca5bff795f7ea4a002
Felix Jablinski <jablinskifelix@gmail.com>
2024-04-17 21:38:51 +0200

    #4 Add pre-game text

--- src/apps/soccer/soccer.py
+++ src/apps/soccer/soccer.py
@@ -1,12 +1,14 @@
 from api.base import Base
 from rgbmatrix import FrameCanvas, graphics
 
+from datetime import datetime
 from PIL import Image
 from dataclasses import dataclass
 from typing import List
 from requests import get
 from io import BytesIO
 
+DATE_FORMAT = "%Y-%m-%dT%H:%MZ"
 ESPN_URL = "https://site.api.espn.com/apis/site/v2/sports/soccer/ger.1/scoreboard"
 FONT_SMALL_NAME = "4x6"
 FONT_NAME = "7x13B"
@@ -17,7 +19,8 @@ class Competitor:
     home: bool
     score: str
     name: str
-    color: str
+    color: graphics.Color
+    text_color: graphics.Color
     logo: str
 
 
@@ -25,6 +28,7 @@ class Competitor:
 class Event:
     clock: str
     state: str
+    date: datetime
     competitors: List[Competitor]
 
 
@@ -40,7 +44,7 @@ class Soccer(Base):
         super().__init__(canvas)
         self.small_font = Base.get_font(FONT_SMALL_NAME)
         self.font = Base.get_font(FONT_NAME)
-        self.text_color = graphics.Color(255, 255, 255)
+        self.white = graphics.Color(255, 255, 255)
         self.index = 0
 
     def run(self) -> bool:
@@ -62,30 +66,42 @@ class Soccer(Base):
             self.small_font,
             0,
             32,
-            self.text_color,
+            self.white,
             league.name.upper(),
         )
 
-        clock = "OVER" if current_event.state == "post" else current_event.clock
+        time = (
+            "OVER"
+            if current_event.state == "post"
+            else (
+                current_event.date.strftime("%b, %H:%M")
+                if current_event.state == "pre"
+                else current_event.clock
+            )
+        )
         graphics.DrawText(
             self.canvas,
             self.small_font,
-            65 - len(clock) * 4,
+            65 - len(time) * 4,
             32,
-            self.text_color,
-            clock,
+            self.white,
+            time,
         )
 
         for competitor in current_event.competitors:
-            color = Soccer.hex_to_rgb(competitor.color)
-
             y_range = range(0, 13) if competitor.home else range(13, 26)
             for y in y_range:
                 for x in range(0, 64):
-                    self.canvas.SetPixel(x, y, color[0], color[1], color[2])
+                    self.canvas.SetPixel(
+                        x,
+                        y,
+                        competitor.color[0],
+                        competitor.color[1],
+                        competitor.color[2],
+                    )
 
             logo_transparent = Image.open(BytesIO(get(competitor.logo).content))
-            logo = Image.new("RGBA", logo_transparent.size, color)
+            logo = Image.new("RGBA", logo_transparent.size, competitor.co)
             logo.paste(logo_transparent, mask=logo_transparent)
 
             image_y = 0 if competitor.home else 13
@@ -97,7 +113,7 @@ class Soccer(Base):
                 self.font,
                 18,
                 text_y,
-                self.text_color,
+                competitor.text_color,
                 competitor.name,
             )
             graphics.DrawText(
@@ -105,7 +121,7 @@ class Soccer(Base):
                 self.font,
                 54,
                 text_y,
-                self.text_color,
+                competitor.text_color,
                 competitor.score,
             )
 
@@ -127,6 +143,7 @@ class Soccer(Base):
         event = Event(
             event_json["status"]["displayClock"],
             event_json["status"]["type"]["state"],
+            datetime(event_json["date"], DATE_FORMAT),
             [],
         )
 
@@ -137,11 +154,15 @@ class Soccer(Base):
 
     @staticmethod
     def load_competitor(competitor_json: dict) -> Competitor:
+        color = Soccer.hex_to_rgb(competitor_json["team"]["color"])
+        text_color = Soccer.hex_to_rgb(competitor_json["team"]["alternateColor"])
+
         return Competitor(
             competitor_json["homeAway"] == "home",
             competitor_json["score"],
             competitor_json["team"]["abbreviation"],
-            competitor_json["team"]["color"],
+            graphics.Color(color[0], color[1], color[2]),
+            graphics.Color(text_color[0], text_color[1], text_color[2]),
             competitor_json["team"]["logo"].replace(
                 "https://a.espncdn.com/", "https://a.espncdn.com/combiner/i?img="
             )
