c5db09553f747a339a3b7cf6a6e6e52d8cc74003
Felix Jablinski <jablinskifelix@gmail.com>
2024-04-19 17:30:33 +0200

    #1 Add api/device/brightness

--- src/api/routes/device.py
+++ src/api/routes/device.py
@@ -1,8 +1,10 @@
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter, HTTPException, Path
 from socket import gethostname
 from subprocess import Popen, PIPE
+from typing import Annotated
 
 from config import LED_BRIGHTNESS
+from api.routes.apps import app_handler
 
 
 router = APIRouter()
@@ -18,9 +20,17 @@ def update():
     process = Popen(["git", "pull"], stdout=PIPE)
 
     try:
-        output = process.communicate(timeout=10)[0]
+        output = process.communicate(timeout=10)[1]
         process.kill()
 
-        return output
+        return output != b""
     except:
         raise HTTPException(status_code=500, detail="Can't update repository.")
+
+
+@router.post("/brightness")
+def brightness(
+    brightness: Annotated[int, Path(title="The ID of the item to get", ge=1, le=100)]
+):
+    app_handler.matrix.brightness = brightness
+    return True
