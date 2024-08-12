fdaf1cb224d73d5f18ce9a7ba25f708625f37b73
Felix Jablinski <jablinskifelix@gmail.com>
2024-08-12 18:34:42 +0200

    Fix workind directory in init script

--- init.sh
+++ init.sh
@@ -3,16 +3,16 @@
 # Exit immediately if a command exits with a non-zero status
 set -e
 
-echo -n "Cloning submodules..."
+echo -n "Cloning submodules... "
 git submodule update --init --recursive &>/dev/null
 echo "Done."
 
 echo -n "Updating package list and installing dependencies... "
 cd rpi-rgb-led-matrix &>/dev/null
-echo "Done."
 
 sudo apt-get update &>/dev/null
 sudo apt-get install -y python3-dev python3-pillow &>/dev/null
+echo "Done."
 
 echo -n "Building and installing Python bindings... "
 make build-python PYTHON=$(command -v python3) &>/dev/null
@@ -28,7 +28,7 @@ echo "Done."
 SERVICE_NAME="infomate"
 COMMAND_TO_RUN="sudo uvicorn main:app --host 0.0.0.0 --port 8000"
 
-WORKING_DIR="$HOME/Desktop/Infomate/src"
+WORKING_DIR="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )/src"
 SERVICE_FILE="/etc/systemd/system/$SERVICE_NAME.service"
 
 sudo bash -c "cat > $SERVICE_FILE" <<EOL
