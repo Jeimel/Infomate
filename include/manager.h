1da30eaae87fdaaa1983025d1125a593e602c95d
Felix Jablinski <jablinskifelix@gmail.com>
2025-11-22 19:31:21 +0100

    Add `AppManager` to handle instances of `App`

---
+++ include/manager.h
@@ -0,0 +1,42 @@
+#include <condition_variable>
+#include <memory>
+#include <mutex>
+
+#include "app.h"
+#include "port.h"
+
+namespace Infomate {
+
+class AppManager : public port::InputPort {
+   public:
+    explicit AppManager(std::unique_ptr<port::DisplayPort> display)
+        : display(std::move(display)), running(false), brightness(100) {}
+
+    ~AppManager() { stop(); }
+
+    void start();
+    void stop();
+
+    bool swap(const std::string& appId) override;
+    bool configure(const app::ConfigMap& config) override;
+    std::string current() const override;
+
+    int getBrightness() const override { return brightness; }
+    void setBrightness(int b) override { brightness = std::clamp(b, 0, 100); }
+
+   private:
+    void loop();
+
+    std::unique_ptr<port::DisplayPort> display;
+    std::unique_ptr<app::App> app;
+
+    std::condition_variable cv;
+    mutable std::mutex mutex;
+
+    std::atomic<bool> running;
+    std::atomic<bool> changed;
+
+    std::atomic<int> brightness;
+};
+
+}  // namespace Infomate
