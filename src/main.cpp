48234e75dfe753ff8a7f49d88b40cc66eb2dc0c4
Felix Jablinski <jablinskifelix@gmail.com>
2025-12-10 16:25:18 +0100

    Add optional matrix hardware support

--- src/main.cpp
+++ src/main.cpp
@@ -3,19 +3,28 @@
 #include <thread>
 
 #include "adapter/api.h"
-#include "adapter/terminal.h"
 #include "apps.h"
 #include "manager.h"
 
+#ifdef USE_RGB_MATRIX
+#include "adapter/matrix.h"
+#else
+#include "adapter/terminal.h"
+#endif
+
 int main() {
     const int apiPort = 18080;
-    const std::string initialApp = "dvd";
+    const std::string initialApp = "clock";
 
+#ifdef USE_RGB_MATRIX
+    auto display = std::make_unique<Infomate::adapter::MatrixAdapter>();
+#else
     auto display = std::make_unique<Infomate::adapter::TerminalAdapter>();
+#endif
 
     Infomate::AppManager manager(std::move(display));
-    Infomate::adapter::CrowAdapter api(manager, apiPort);
 
+    Infomate::adapter::CrowAdapter api(manager, apiPort);
     auto api_thread = std::jthread([&api]() { api.run(); });
 
     manager.swap(initialApp);
